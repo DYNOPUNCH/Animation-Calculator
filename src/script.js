02f8ce41a2e92c9cce8b678aa5b79be75327f023
DYNOPUNCH <dynopunch101@gmail.com>
2024-06-23 11:13:32 -0400

    added entry validation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,26 @@
 var button = document.getElementById("result");
 var release = true;
 
+// Checks if a variable is null or empty.
+function validate(a)
+{
+    if(a == null || a == "")
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
+// Checks if input is a number.
+function isNumber(value) 
+{
+    return typeof value === 'number';
+}
+
+// Calculate the Animation Frames
+
 function calculateFrames()
 {
 
@@ -11,14 +31,32 @@ function calculateFrames()
     let seconds = document.getElementById("Second_l").value;
     let milliseconds = document.getElementById("Milli_l").value;
 
-    // TODO Add check that sees if all 3 boxes are empty, and if so add a warning message that says to do so.
+    // Check fps box
 
-    // Check if Debug or release.
-
-    if(release == false)
+    if(validate(fps))
     {
-        console.log("fps", fps);
-        console.log("minuets:", minuets, "seconds:", seconds, "milliseconds:", milliseconds);
+        alert("Please fill in the FPS field");
+        return;
+    }
+
+    if(isNaN(fps))
+    {
+        alert("Entry must be a number.");
+        return;
+    }
+
+    // Check time box
+
+    if(validate(minuets) && validate(seconds) && validate(milliseconds))
+    {
+        alert("Please fill in atleast one of the time fields");
+        return;
+    }
+
+    if(isNaN(minuets) || isNaN(seconds) || isNaN(milliseconds))
+    {
+        alert("Entry must be a number.");
+        return;
     }
 
     // Calculate the frames per seconds.
