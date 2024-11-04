4841d80df20d12062e4c2e3f45e97a7895245f32
DYNO <dynopunch101@gmail.com>
2024-11-04 18:16:00 -0500

    selection box added with temporary method

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("result");
 var release = true;
 var buttonSubmit = document.getElementById("submitButton");
+var fps = 12;
 
 // Checks if a variable is null or empty.
 function validate(a)
@@ -20,31 +21,29 @@ function isNumber(value)
     return typeof value === 'number';
 }
 
+// Check if the fps has been changed.
+var fps_e = document.getElementById("fps_l");
+
+function onChange()
+{
+    var value = fps_e.value;
+    var number = fps_e.options[fps_e.selectedIndex].value;
+    fps = fps_e.options[fps_e.selectedIndex].value;
+    console.log(value, number);
+}
+
+fps_e.onchange = onChange;
+
 // Calculate the Animation Frames
 
 function calculateFrames()
 {
 
-    // Obtain all of the values from the html.
+    // Obtain all the values from the html.
 
-    let fps = document.getElementById("fps_l").value;
     let minuets = document.getElementById("Minuet_l").value;
     let seconds = document.getElementById("Second_l").value;
     let milliseconds = document.getElementById("Milli_l").value;
-
-    // Check fps box
-
-    if(validate(fps))
-    {
-        alert("Please fill in the FPS field");
-        return;
-    }
-
-    if(isNaN(fps))
-    {
-        alert("Entry must be a number.");
-        return;
-    }
 
     // Check time box
 
@@ -61,9 +60,13 @@ function calculateFrames()
     }
 
     // Calculate the frames per seconds.
-    
+
+
+
     button.innerHTML = fps * seconds + fps * (milliseconds / 1000) + fps * (minuets * 60);
 }
+
+
 
 $("#submitButton").on('click', calculateFrames);
 
